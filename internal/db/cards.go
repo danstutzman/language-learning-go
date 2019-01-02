d85850f7e1d883ccb311ad6dbda8b5a1414c31db
Dan Stutzman <dan@danstutzman.com>
2019-01-02 15:08:23 -0700

    Add cards.es_json column

--- internal/db/cards.go
+++ internal/db/cards.go
@@ -8,10 +8,11 @@ import (
 type Card struct {
 	CardId int    `json:"cardId"`
 	EsText string `json:"esText"`
+	EsJson string `json:"esJson"`
 }
 
 func AssertCardsHasCorrectSchema(db *sql.DB) {
-	stmt, err := db.Prepare("select card_id, es_text from cards limit 1")
+	stmt, err := db.Prepare("select card_id, es_text, es_json from cards limit 1")
 	if err != nil {
 		log.Fatalf("Error from db.Prepare: %s", err)
 	}
@@ -21,7 +22,7 @@ func AssertCardsHasCorrectSchema(db *sql.DB) {
 func SelectAllFromCards(db *sql.DB) []Card {
 	cards := []Card{}
 
-	rows, err := db.Query("select card_id, es_text from cards")
+	rows, err := db.Query("select card_id, es_text, es_json from cards")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,7 +30,7 @@ func SelectAllFromCards(db *sql.DB) []Card {
 
 	for rows.Next() {
 		var card Card
-		err = rows.Scan(&card.CardId, &card.EsText)
+		err = rows.Scan(&card.CardId, &card.EsText, &card.EsJson)
 		if err != nil {
 			log.Fatal(err)
 		}
