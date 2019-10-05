bdea6e84be5753d12888d7d7812232b79e5726da
Dan Stutzman <dan@danstutzman.com>
2019-10-05 15:03:48 -0600

    Assume IndexedDB on browser is generating all the IDs

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -6,15 +6,15 @@ rm -f db.sqlite3
 
 sqlite3 db.sqlite3 <<EOF
   CREATE TABLE cards (
-    id                INTEGER PRIMARY KEY NOT NULL,
+    id                INTEGER NOT NULL,
     l1                TEXT NOT NULL,
     l2                TEXT NOT NULL,
     morpheme_ids_json TEXT NOT NULL,
     created_at_millis REAL NOT NULL,
     updated_at_millis REAL NOT NULL
   );
-  INSERT INTO cards(l1, l2, morpheme_ids_json, created_at_millis, updated_at_millis)
-    VALUES('man', 'hombre', '[]', STRFTIME('%s','now'), STRFTIME('%s','now'));
+  INSERT INTO cards(id, l1, l2, morpheme_ids_json, created_at_millis, updated_at_millis)
+    VALUES(-1, 'man', 'hombre', '[]', STRFTIME('%s','now'), STRFTIME('%s','now'));
 EOF
 
 echo '.schema' | sqlite3 db.sqlite3
