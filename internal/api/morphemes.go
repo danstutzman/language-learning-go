1c54c068779be900da15dd49954a95bd5be32099
Dan Stutzman <dan@danstutzman.com>
2019-10-06 23:15:06 -0600

    Parse phrase into morphemes if given l2_phrase param

--- internal/api/morphemes.go
+++ internal/api/morphemes.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"regexp"
-	"strings"
 )
 
 type ListMorphemesResponse struct {
@@ -21,43 +20,46 @@ func (api *Api) HandleListMorphemesRequest(w http.ResponseWriter, r *http.Reques
 	setCORSHeaders(w)
 	w.Header().Set("Content-Type", "application/json; charset=\"utf-8\"")
 
-	where := ""
-	l2Prefix, ok := r.URL.Query()["l2_prefix"]
-	if ok && l2Prefix[0] != "" {
-		if where == "" {
-			where += "WHERE "
-		} else {
-			where += " AND "
-		}
-		where += "l2 LIKE " + db.Escape(l2Prefix[0]+"%")
-	}
+	var morphemes []db.MorphemeRow
+	l2Phrase, ok := r.URL.Query()["l2_phrase"]
+	if ok && l2Phrase[0] != "" {
+		morphemes = []db.MorphemeRow{}
+		for _, word := range L2_WORD_REGEXP.FindAllString(l2Phrase[0], -1) {
+			exactMatches := db.FromMorphemes(api.db, "WHERE l2 = "+db.Escape(word))
+			if len(exactMatches) > 0 {
+				morphemes = append(morphemes, exactMatches...)
+			} else {
+				// look for matches with two morphemes
+				prefixMatches := db.FromMorphemes(api.db,
+					"WHERE "+db.Escape(word)+" LIKE (RTRIM(l2, '-') || '%')")
+				for _, prefixMatch := range prefixMatches {
+					// subtract one to account for the prefix's hyphen
+					suffix := "-" + word[(len(prefixMatch.L2)-1):]
 
-	allL2, ok := r.URL.Query()["all_l2"]
-	if ok && allL2[0] != "" {
-		words := []string{}
-		for _, word := range L2_WORD_REGEXP.FindAllString(allL2[0], -1) {
-			words = append(words, strings.ToLower(word))
-		}
-
-		if len(words) > 0 {
-			likes := []string{}
-			for _, word := range words {
-				like := db.Escape(word) + " LIKE (RTRIM(l2, '-') || '%')"
-				likes = append(likes, like)
+					suffixMatches := db.FromMorphemes(api.db, "WHERE l2 = "+db.Escape(suffix))
+					if len(suffixMatches) > 0 {
+						morphemes = append(morphemes, prefixMatch)
+						morphemes = append(morphemes, suffixMatches...)
+					}
+				}
 			}
-
+		}
+	} else {
+		where := ""
+		l2Prefix, ok := r.URL.Query()["l2_prefix"]
+		if ok && l2Prefix[0] != "" {
 			if where == "" {
 				where += "WHERE "
 			} else {
 				where += " AND "
 			}
-			where += strings.Join(likes, " OR ")
+			where += "l2 LIKE " + db.Escape(l2Prefix[0]+"%")
 		}
+
+		morphemes = db.FromMorphemes(api.db, where+" limit 20")
 	}
 
-	response := ListMorphemesResponse{
-		Morphemes: db.FromMorphemes(api.db, where+" limit 20"),
-	}
+	response := ListMorphemesResponse{Morphemes: morphemes}
 	bytes, err := json.Marshal(response)
 	if err != nil {
 		log.Fatalf("Error from json.Marshal: %s", err)
