d4caced41c563184c8d6d433ccb557e9e579b616
Dan Stutzman <dan@danstutzman.com>
2019-10-08 12:32:18 -0600

    Fix: deleting and recreating database caused stale reads

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS cards;
 CREATE TABLE cards (
   id                INTEGER PRIMARY KEY NOT NULL,
   l1                TEXT NOT NULL,
@@ -6,6 +7,7 @@ CREATE TABLE cards (
 CREATE INDEX idx_cards_l1 ON cards(l1);
 CREATE INDEX idx_cards_l2 ON cards(l2);
 
+DROP TABLE IF EXISTS cards_morphemes;
 CREATE TABLE cards_morphemes (
   card_id INTEGER NOT NULL,
   morpheme_id INTEGER NOT NULL,
@@ -14,6 +16,7 @@ CREATE TABLE cards_morphemes (
 CREATE INDEX idx_cards_morphemes_card_id ON cards_morphemes(card_id);
 CREATE INDEX idx_cards_morphemes_morpheme_id ON cards_morphemes(morpheme_id);
 
+DROP TABLE IF EXISTS morphemes;
 CREATE TABLE morphemes (
   id                INTEGER PRIMARY KEY NOT NULL,
   l2                TEXT NOT NULL,
