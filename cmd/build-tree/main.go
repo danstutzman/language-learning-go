68e3fcf4aa7274e00ab06bfddd3bfece429052e1
Dan Stutzman <dan@danstutzman.com>
2019-10-18 13:02:21 -0600

    Execute string commands instead of Ops

--- cmd/build-tree/main.go
+++ cmd/build-tree/main.go
@@ -2,48 +2,51 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-type Op struct {
-	type_ string
-	l2    string
-	l1    string
-}
+const commands = `
+ADD_VERB_INFINITIVE/busc-/seek
+ADD_VERB_UNIQUE/está/is
+MAKE_PRES_PROG/-ando/-ing
+ADD_NOUN/Apple/Apple
+MAKE_AGENT
+ADD_VERB_INFINITIVE/compr-/buy
+MAKE_INFINITIVE/-ar/to
+ADD_NOUN/startup/startup
+ADD_DET/una/a
+MAKE_DET_NOUN
+ADD_NOUN/Reino/Kingdom
+ADD_ADJ/Unido/United
+MAKE_NOUN_ADJ
+ADD_DET/el/the
+MAKE_DET_NOUN
+ADD_PREP/de/from
+MAKE_PREP_NOUN
+MAKE_NOUN_PHRASE_ADDING_PREP_PHRASE
+MAKE_DOBJ
+ADD_NOUN/millón/million
+MAKE_PLURAL/-es/-s
+ADD_NOUN/dólar/dollar
+MAKE_PLURAL/-es/-s
+ADD_PREP/de/of
+MAKE_PREP_NOUN
+MAKE_NOUN_PHRASE_ADDING_PREP_PHRASE
+ADD_PREP/por/for
+MAKE_PREP_NOUN
+MAKE_VERB_PHRASE_ADDING_PREP_PHRASE
+MAKE_COMPOUND_VERB
+`
 
 func main() {
 	stack := Stack{}
-	stack.exec(Op{type_: "ADD_VERB_INFINITIVE", l2: "busc-", l1: "seek"})
-	stack.exec(Op{type_: "ADD_VERB_UNIQUE", l2: "está", l1: "is"})
-	stack.exec(Op{type_: "MAKE_PRES_PROG", l2: "-ando", l1: "-ing"})
-	stack.exec(Op{type_: "ADD_NOUN", l2: "Apple", l1: "Apple"})
-	stack.exec(Op{type_: "MAKE_AGENT", l2: "", l1: ""})
-	stack.exec(Op{type_: "ADD_VERB_INFINITIVE", l2: "compr-", l1: "buy"})
-	stack.exec(Op{type_: "MAKE_INFINITIVE", l2: "-ar", l1: "to"})
-	stack.exec(Op{type_: "ADD_NOUN", l2: "startup", l1: "startup"})
-	stack.exec(Op{type_: "ADD_DET", l2: "una", l1: "a"})
-	stack.exec(Op{type_: "MAKE_DET_NOUN", l2: "", l1: ""})
-	stack.exec(Op{type_: "ADD_NOUN", l2: "Reino", l1: "Kingdom"})
-	stack.exec(Op{type_: "ADD_ADJ", l2: "Unido", l1: "United"})
-	stack.exec(Op{type_: "MAKE_NOUN_ADJ", l2: "", l1: ""})
-	stack.exec(Op{type_: "ADD_DET", l2: "el", l1: "the"})
-	stack.exec(Op{type_: "MAKE_DET_NOUN", l2: "", l1: ""})
-	stack.exec(Op{type_: "ADD_PREP", l2: "de", l1: "from"})
-	stack.exec(Op{type_: "MAKE_PREP_NOUN", l2: "", l1: ""})
-	stack.exec(Op{type_: "MAKE_NOUN_PHRASE_ADDING_PREP_PHRASE", l2: "", l1: ""})
-	stack.exec(Op{type_: "MAKE_DOBJ", l2: "", l1: ""})
-	stack.exec(Op{type_: "ADD_NOUN", l2: "millón", l1: "million"})
-	stack.exec(Op{type_: "MAKE_PLURAL", l2: "-es", l1: "-s"})
-	stack.exec(Op{type_: "ADD_NOUN", l2: "dólar", l1: "dollar"})
-	stack.exec(Op{type_: "MAKE_PLURAL", l2: "-es", l1: "-s"})
-	stack.exec(Op{type_: "ADD_PREP", l2: "de", l1: "of"})
-	stack.exec(Op{type_: "MAKE_PREP_NOUN", l2: "", l1: ""})
-	stack.exec(Op{type_: "MAKE_NOUN_PHRASE_ADDING_PREP_PHRASE", l2: "", l1: ""})
-	stack.exec(Op{type_: "ADD_PREP", l2: "por", l1: "for"})
-	stack.exec(Op{type_: "MAKE_PREP_NOUN", l2: "", l1: ""})
-	stack.exec(Op{type_: "MAKE_VERB_PHRASE_ADDING_PREP_PHRASE", l2: "", l1: ""})
-	stack.exec(Op{type_: "MAKE_COMPOUND_VERB", l2: "", l1: ""})
+	for _, commandWhitespace := range strings.Split(commands, "\n") {
+		command := strings.TrimSpace(commandWhitespace)
+		if command != "" {
+			stack.execCommand(command)
+		}
+	}
 
-	fmt.Printf("%+v\n", stack)
 	fmt.Printf("%+v\n", stack.getL1Words())
 	fmt.Printf("%+v\n", stack.getL2Words())
 }
--- cmd/build-tree/stack.go
+++ cmd/build-tree/stack.go
@@ -1,82 +1,131 @@
 package main
 
+import (
+	"log"
+	"strings"
+)
+
 type Stack struct {
 	constituents []Constituent
 }
 
-func (stack *Stack) exec(op Op) {
-	if op.type_ == "ADD_VERB_INFINITIVE" ||
-		op.type_ == "ADD_VERB_UNIQUE" ||
-		op.type_ == "ADD_NOUN" ||
-		op.type_ == "ADD_DET" ||
-		op.type_ == "ADD_ADJ" ||
-		op.type_ == "ADD_PREP" {
-		stack.push(Constituent{l1: op.l1, l2: op.l2})
-		return
-	} else if op.type_ == "MAKE_PRES_PROG" {
-		estar := stack.pop()
-		infinitive := stack.peek()
-		infinitive.prependL2Prefix(estar.l2)
-		infinitive.prependL1Prefix(estar.l1)
-		infinitive.appendL2Suffix(op.l2)
-		infinitive.appendL1Suffix(op.l1)
-		return
-	} else if op.type_ == "MAKE_AGENT" {
-		agent := stack.pop()
-		lastConstituent := stack.peek()
-		lastConstituent.leftChildren =
-			append([]Constituent{agent}, lastConstituent.leftChildren...)
-		return
-	} else if op.type_ == "MAKE_DET_NOUN" {
-		det := stack.pop()
-		noun := stack.peek()
-		noun.prependL2Prefix(det.l2)
-		noun.prependL1Prefix(det.l1)
-		return
-	} else if op.type_ == "MAKE_NOUN_ADJ" {
-		adj := stack.pop()
-		noun := stack.peek()
-		noun.appendRightChild(adj)
-		return
-	} else if op.type_ == "MAKE_PREP_NOUN" {
-		prep := stack.pop()
-		noun := stack.peek()
-		noun.prependL2Prefix(prep.l2)
-		noun.prependL1Prefix(prep.l1)
-		return
-	} else if op.type_ == "MAKE_NOUN_PHRASE_ADDING_PREP_PHRASE" ||
-		op.type_ == "MAKE_VERB_PHRASE_ADDING_PREP_PHRASE" {
-		prepPhrase := stack.pop()
-		growingPhrase := stack.peek()
-		growingPhrase.appendRightChild(prepPhrase)
-		return
-	} else if op.type_ == "MAKE_DOBJ" {
-		nounPhrase := stack.pop()
-		verbPhrase := stack.peek()
-		verbPhrase.appendRightChild(nounPhrase)
-		return
-	} else if op.type_ == "MAKE_PLURAL" {
-		noun := stack.peek()
-		noun.appendL2Suffix("-es")
-		noun.appendL1Suffix("-s")
-		return
-	} else if op.type_ == "MAKE_INFINITIVE" {
-		noun := stack.peek()
-		noun.appendL2Suffix("-ar")
-		noun.prependL1Prefix("to")
-		return
-	} else if op.type_ == "MAKE_COMPOUND_VERB" {
-		verbPhraseToAdd := stack.pop()
-		verbPhraseToGrow := stack.peek()
-		verbPhraseToGrow.appendRightChild(verbPhraseToAdd)
-		return
-	} else {
-		panic("Unknown op.type " + op.type_)
+func expectNumArgs(expectedNum int, args []string) {
+	if len(args) != expectedNum+1 {
+		log.Fatalf("Expected %d args but got %v", expectedNum, args)
 	}
 }
 
-func (stack *Stack) push(newConstituent Constituent) {
-	stack.constituents = append(stack.constituents, newConstituent)
+func (stack *Stack) execCommand(command string) {
+	args := strings.Split(command, "/")
+	switch args[0] {
+	case "ADD_ADJ", "ADD_DET", "ADD_NOUN", "ADD_PREP", "ADD_VERB_INFINITIVE",
+		"ADD_VERB_UNIQUE":
+		expectNumArgs(2, args)
+		stack.push(args[1], args[2])
+	case "MAKE_AGENT":
+		expectNumArgs(0, args)
+		stack.makeAgent()
+	case "MAKE_COMPOUND_VERB":
+		expectNumArgs(0, args)
+		stack.makeCompoundVerb()
+	case "MAKE_DET_NOUN":
+		expectNumArgs(0, args)
+		stack.makeDetNoun()
+	case "MAKE_INFINITIVE":
+		expectNumArgs(2, args)
+		stack.makeInfinitive(args[1], args[2])
+	case "MAKE_NOUN_ADJ":
+		expectNumArgs(0, args)
+		stack.makeNounAdj()
+	case "MAKE_PLURAL":
+		expectNumArgs(2, args)
+		stack.makePlural(args[1], args[2])
+	case "MAKE_PREP_NOUN":
+		expectNumArgs(0, args)
+		stack.makePrepNoun()
+	case "MAKE_PRES_PROG":
+		expectNumArgs(2, args)
+		stack.makePresProg(args[1], args[2])
+	case "MAKE_DOBJ":
+		expectNumArgs(0, args)
+		stack.makeDirObj()
+	case "MAKE_NOUN_PHRASE_ADDING_PREP_PHRASE",
+		"MAKE_VERB_PHRASE_ADDING_PREP_PHRASE":
+		expectNumArgs(0, args)
+		stack.makePhraseAddingPrepPhrase()
+	default:
+		panic("Unknown command " + command)
+	}
+}
+
+func (stack *Stack) push(l2, l1 string) {
+	stack.constituents = append(stack.constituents, Constituent{l2: l2, l1: l1})
+}
+
+func (stack *Stack) makeAgent() {
+	agent := stack.pop()
+	lastConstituent := stack.peek()
+	lastConstituent.leftChildren =
+		append([]Constituent{agent}, lastConstituent.leftChildren...)
+}
+
+func (stack *Stack) makeCompoundVerb() {
+	verbPhraseToAdd := stack.pop()
+	verbPhraseToGrow := stack.peek()
+	verbPhraseToGrow.appendRightChild(verbPhraseToAdd)
+}
+
+func (stack *Stack) makeDetNoun() {
+	det := stack.pop()
+	noun := stack.peek()
+	noun.prependL2Prefix(det.l2)
+	noun.prependL1Prefix(det.l1)
+}
+
+func (stack *Stack) makeInfinitive(l2, l1 string) {
+	noun := stack.peek()
+	noun.appendL2Suffix("-ar")
+	noun.prependL1Prefix("to")
+}
+
+func (stack *Stack) makeNounAdj() {
+	adj := stack.pop()
+	noun := stack.peek()
+	noun.appendRightChild(adj)
+}
+
+func (stack *Stack) makeDirObj() {
+	nounPhrase := stack.pop()
+	verbPhrase := stack.peek()
+	verbPhrase.appendRightChild(nounPhrase)
+}
+
+func (stack *Stack) makePhraseAddingPrepPhrase() {
+	prepPhrase := stack.pop()
+	growingPhrase := stack.peek()
+	growingPhrase.appendRightChild(prepPhrase)
+}
+
+func (stack *Stack) makePlural(l2, l1 string) {
+	noun := stack.peek()
+	noun.appendL2Suffix(l2)
+	noun.appendL1Suffix(l1)
+}
+
+func (stack *Stack) makePrepNoun() {
+	prep := stack.pop()
+	noun := stack.peek()
+	noun.prependL2Prefix(prep.l2)
+	noun.prependL1Prefix(prep.l1)
+}
+
+func (stack *Stack) makePresProg(l2, l1 string) {
+	estar := stack.pop()
+	infinitive := stack.peek()
+	infinitive.prependL2Prefix(estar.l2)
+	infinitive.prependL1Prefix(estar.l1)
+	infinitive.appendL2Suffix(l2)
+	infinitive.appendL1Suffix(l1)
 }
 
 func (stack *Stack) peek() *Constituent {
@@ -109,4 +158,9 @@ func (stack *Stack) getL2Words() []string {
 		l2Words = append(l2Words, constituent.getL2Words()...)
 	}
 	return l2Words
+}
+
+func (stack *Stack) pushConstituent(l2, l1 string) {
+	stack.constituents =
+		append(stack.constituents, Constituent{l2: l2, l1: l1})
 }
