527ffa34e0a8759fc8f93c0bd275c62742301457
Dan Stutzman <dan@danstutzman.com>
2019-10-23 19:40:44 -0600

    -ar verbs look up group_infinitive_stems, suffix_rules

--- internal/freeling/print_exceptions.go
+++ internal/freeling/print_exceptions.go
@@ -40,7 +40,7 @@ func PrintVerbExceptions(freelingDiccPath string) {
 			tag := values[i+1]
 
 			expected := true
-			if false && strings.HasSuffix(lemma, "ar") && strings.HasPrefix(tag, "V") {
+			if strings.HasSuffix(lemma, "ar") && strings.HasPrefix(tag, "V") {
 				conjugations := analyzeArVerb(lemma, tag)
 				expected = false
 				for _, conjugation := range conjugations {
@@ -63,7 +63,7 @@ func PrintVerbExceptions(freelingDiccPath string) {
 					}
 				}
 			}
-			if strings.HasSuffix(lemma, "ir") && strings.HasPrefix(tag, "V") {
+			if false && strings.HasSuffix(lemma, "ir") && strings.HasPrefix(tag, "V") {
 				conjugations := analyzeIrVerb(lemma, tag)
 				expected = false
 				for _, conjugation := range conjugations {
@@ -85,54 +85,39 @@ func PrintVerbExceptions(freelingDiccPath string) {
 }
 
 func analyzeArVerb(lemma, tag string) []Conjugation {
+	groupInfinitiveStems := groupInfinitiveStemsByInfinitive[lemma]
+
+	groups := map[string]bool{}
+	for _, groupInfinitiveStem := range groupInfinitiveStems {
+		groups[groupInfinitiveStem.group] = true
+	}
+
+	defaultStem := lemma[0 : len(lemma)-2]
+	groups["AR"] = true
+
+	groupTag27Suffixes := findGroupTag27Suffixes(groups, tag[2:7])
+
 	conjugations := []Conjugation{}
-	suffixes := AR_MUTANT_TAG27_TO_SUFFIXES[tag[2:7]]
-	if len(suffixes) > 0 && AR_MUTANTS[lemma] != "" {
-		stem := AR_MUTANTS[lemma]
-
-		for _, suffix := range suffixes {
-			conjugation := Conjugation{stem: stem, suffix: suffix}
-			conjugations = append(conjugations, conjugation)
-		}
-	} else {
-		stems := AR_STEM_CHANGES[lemma]
-		if len(stems) == 0 || !AR_TAG27_TO_STEM_CHANGE[tag[2:7]] {
-			stems = []string{lemma[0 : len(lemma)-2]}
-		}
-
-		suffixes = AR_TAG27_TO_SUFFIXES[tag[2:7]]
-
-		for _, stem := range stems {
-			for _, suffix := range suffixes {
-				if suffix == "e" || suffix == "en" || suffix == "é" ||
-					suffix == "emos" || suffix == "es" || suffix == "éis" {
-					stem = ENDS_WITH_C.ReplaceAllString(stem, "qu")
-					stem = ENDS_WITH_GU.ReplaceAllString(stem, "gü")
-					stem = ENDS_WITH_G.ReplaceAllString(stem, "gu")
-					stem = ENDS_WITH_Z.ReplaceAllString(stem, "c")
-				}
-
-				if lemma == "estar" {
-					if suffix == "a" {
-						suffix = "á"
-					} else if suffix == "an" {
-						suffix = "án"
-					} else if suffix == "as" {
-						suffix = "ás"
-					} else if suffix == "e" {
-						suffix = "é"
-					} else if suffix == "en" {
-						suffix = "én"
-					} else if suffix == "es" {
-						suffix = "és"
-					}
-				}
-
-				conjugation := Conjugation{stem: stem, suffix: suffix}
-				conjugations = append(conjugations, conjugation)
+	for _, groupTag27Suffix := range groupTag27Suffixes {
+		stemsForGroup := []string{}
+		for _, groupInfinitiveStem := range groupInfinitiveStems {
+			if groupInfinitiveStem.group == groupTag27Suffix.group {
+				stemsForGroup = append(stemsForGroup, groupInfinitiveStem.stem)
 			}
 		}
+		if len(stemsForGroup) == 0 {
+			stemsForGroup = []string{defaultStem}
+		}
+
+		for _, stem := range stemsForGroup {
+			conjugation := Conjugation{
+				stem:   stem,
+				suffix: groupTag27Suffix.suffix,
+			}
+			conjugations = append(conjugations, conjugation)
+		}
 	}
+
 	return conjugations
 }
 
