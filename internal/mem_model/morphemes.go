3474b3844356f2f9766c8faf537bb6ec7fa77966
Dan Stutzman <dan@danstutzman.com>
2019-10-27 22:27:12 -0600

    Split gender/number suffix from nouns and adjectives

--- internal/mem_model/morphemes.go
+++ internal/mem_model/morphemes.go
@@ -8,6 +8,7 @@ import (
 	"database/sql"
 	"fmt"
 	"gopkg.in/guregu/null.v3"
+	"strings"
 )
 
 type Morpheme struct {
@@ -24,74 +25,180 @@ func (memModel *MemModel) getNextMorphemeId() int {
 	return id
 }
 
+func (memModel *MemModel) VerbTokenToCard(token parsing.Token) (Card, error) {
+	conjugations := freeling.AnalyzeVerb(token.Lemma, token.Tag)
+	if len(conjugations) == 0 {
+		return Card{}, fmt.Errorf("Can't conjugate verb %v", token)
+	}
+	conjugation := conjugations[0]
+
+	var morphemes []Morpheme
+	if conjugation.Suffix == "" {
+		morpheme, exists := memModel.morphemeByL2Tag[token.Form+token.Tag]
+		if !exists {
+			morpheme = Morpheme{
+				Id:    memModel.getNextMorphemeId(),
+				Type:  "VERB_UNIQUE",
+				L2:    conjugation.Stem,
+				Lemma: null.StringFrom(token.Lemma),
+				Tag:   null.StringFrom(token.Tag),
+			}
+			memModel.morphemes = append(memModel.morphemes, morpheme)
+			memModel.morphemeByL2Tag[token.Form+token.Tag] = morpheme
+		}
+		morphemes = []Morpheme{morpheme}
+	} else {
+		stemMorpheme, exists := memModel.morphemeByL2Tag[conjugation.Stem+""]
+		if !exists {
+			stemMorpheme = Morpheme{
+				Id:    memModel.getNextMorphemeId(),
+				Type:  "VERB_STEM",
+				L2:    conjugation.Stem,
+				Lemma: null.StringFrom(token.Lemma),
+				Tag:   null.StringFrom(token.Tag),
+			}
+			memModel.morphemes = append(memModel.morphemes, stemMorpheme)
+			memModel.morphemeByL2Tag[conjugation.Stem+""] = stemMorpheme
+		}
+
+		suffixMorpheme, exists :=
+			memModel.morphemeByL2Tag[conjugation.Suffix+token.Tag]
+		if !exists {
+			suffixMorpheme = Morpheme{
+				Id:    memModel.getNextMorphemeId(),
+				Type:  "VERB_SUFFIX",
+				L2:    conjugation.Suffix,
+				Lemma: null.StringFrom(token.Lemma),
+				Tag:   null.StringFrom(token.Tag),
+			}
+			memModel.morphemes = append(memModel.morphemes, suffixMorpheme)
+			memModel.morphemeByL2Tag[conjugation.Suffix+token.Tag] = suffixMorpheme
+		}
+		morphemes = []Morpheme{stemMorpheme, suffixMorpheme}
+	}
+
+	card, exists := memModel.cardByL2[token.Form]
+	if !exists {
+		card = Card{
+			Id:        memModel.getNextCardId(),
+			Type:      "VERB",
+			L2:        token.Form,
+			Morphemes: morphemes,
+		}
+	}
+	return card, nil
+}
+
+func (memModel *MemModel) NounOrAdjectiveTokenToCard(token parsing.Token) (Card,
+	error) {
+	var gender string
+	var number string
+	if token.Tag[0:1] == "A" { // adjective
+		gender = token.Tag[3:4] // "F" or "M" or "C" (common)
+		number = token.Tag[4:5] // "P" or "S"
+	} else if token.Tag[0:1] == "N" { // noun
+		gender = token.Tag[2:3] // "F" or "M" or "C" (common)
+		number = token.Tag[3:4] // "P" or "S" or "N" (invariable)
+	}
+
+	form := token.Form
+
+	var stem string
+	var suffix string
+	if gender == "M" {
+		if number == "S" && strings.HasSuffix(form, "o") {
+			stem = form[0 : len(form)-1]
+			suffix = "o"
+		} else if number == "P" && strings.HasSuffix(form, "os") {
+			stem = form[0 : len(form)-2]
+			suffix = "os"
+		} else if number == "P" && strings.HasSuffix(form, "es") {
+			stem = form[0 : len(form)-2]
+			suffix = "es"
+		}
+	} else if gender == "F" {
+		if number == "S" && strings.HasSuffix(form, "a") {
+			stem = form[0 : len(form)-1]
+			suffix = "a"
+		} else if number == "P" && strings.HasSuffix(form, "as") {
+			stem = form[0 : len(form)-2]
+			suffix = "as"
+		}
+	} else if gender == "C" {
+		if number == "P" && strings.HasSuffix(form, "os") {
+			stem = form[0 : len(form)-2]
+			suffix = "os"
+		} else if number == "P" && strings.HasSuffix(form, "es") {
+			stem = form[0 : len(form)-2]
+			suffix = "es"
+		}
+	}
+
+	var morphemes []Morpheme
+	if stem != "" {
+		stemMorpheme, exists := memModel.morphemeByL2Tag[stem+""]
+		if !exists {
+			stemMorpheme = Morpheme{
+				Id:    memModel.getNextMorphemeId(),
+				Type:  "NOUN_OR_ADJ_STEM",
+				L2:    stem,
+				Lemma: null.StringFrom(token.Lemma),
+				Tag:   null.String{},
+			}
+			memModel.morphemes = append(memModel.morphemes, stemMorpheme)
+			memModel.morphemeByL2Tag[stem+""] = stemMorpheme
+		}
+
+		suffixMorpheme, exists :=
+			memModel.morphemeByL2Tag[suffix+token.Tag]
+		if !exists {
+			suffixMorpheme = Morpheme{
+				Id:    memModel.getNextMorphemeId(),
+				Type:  "NOUN_OR_ADJ_SUFFIX",
+				L2:    suffix,
+				Lemma: null.String{},
+				Tag:   null.StringFrom(token.Tag),
+			}
+			memModel.morphemes = append(memModel.morphemes, suffixMorpheme)
+			memModel.morphemeByL2Tag[suffix+token.Tag] = suffixMorpheme
+		}
+		morphemes = []Morpheme{stemMorpheme, suffixMorpheme}
+
+	} else {
+		morpheme, exists := memModel.morphemeByL2Tag[form+token.Tag]
+		if !exists {
+			morpheme = Morpheme{
+				Id:    memModel.getNextMorphemeId(),
+				Type:  "NOUN_OR_ADJECTIVE",
+				L2:    form,
+				Lemma: null.StringFrom(token.Lemma),
+				Tag:   null.StringFrom(token.Tag),
+			}
+			memModel.morphemes = append(memModel.morphemes, morpheme)
+			memModel.morphemeByL2Tag[form+token.Tag] = morpheme
+		}
+	}
+
+	card, exists := memModel.cardByL2[form]
+	if !exists {
+		card = Card{
+			Id:        memModel.getNextCardId(),
+			Type:      "NOUN_OR_ADJECTIVE",
+			L2:        token.Form,
+			Morphemes: morphemes,
+		}
+	}
+	return card, nil
+}
+
 func (memModel *MemModel) TokenToCard(token parsing.Token) (Card, error) {
 	if token.IsVerb() {
-		conjugations := freeling.AnalyzeVerb(token.Lemma, token.Tag)
-		if len(conjugations) == 0 {
-			return Card{}, fmt.Errorf("Can't conjugate verb %v", token)
-		}
-		conjugation := conjugations[0]
-
-		var morphemes []Morpheme
-		if conjugation.Suffix == "" {
-			morpheme, exists := memModel.morphemeByL2Tag[token.Form+token.Tag]
-			if !exists {
-				morpheme = Morpheme{
-					Id:    memModel.getNextMorphemeId(),
-					Type:  "VERB_UNIQUE",
-					L2:    conjugation.Stem,
-					Lemma: null.StringFrom(token.Lemma),
-					Tag:   null.StringFrom(token.Tag),
-				}
-				memModel.morphemes = append(memModel.morphemes, morpheme)
-				memModel.morphemeByL2Tag[token.Form+token.Tag] = morpheme
-			}
-			morphemes = []Morpheme{morpheme}
-		} else {
-			stemMorpheme, exists := memModel.morphemeByL2Tag[conjugation.Stem+""]
-			if !exists {
-				stemMorpheme = Morpheme{
-					Id:    memModel.getNextMorphemeId(),
-					Type:  "VERB_STEM",
-					L2:    conjugation.Stem,
-					Lemma: null.StringFrom(token.Lemma),
-					Tag:   null.StringFrom(token.Tag),
-				}
-				memModel.morphemes = append(memModel.morphemes, stemMorpheme)
-				memModel.morphemeByL2Tag[conjugation.Stem+""] = stemMorpheme
-			}
-
-			suffixMorpheme, exists :=
-				memModel.morphemeByL2Tag[conjugation.Suffix+token.Tag]
-			if !exists {
-				suffixMorpheme = Morpheme{
-					Id:    memModel.getNextMorphemeId(),
-					Type:  "VERB_SUFFIX",
-					L2:    conjugation.Suffix,
-					Lemma: null.StringFrom(token.Lemma),
-					Tag:   null.StringFrom(token.Tag),
-				}
-				memModel.morphemes = append(memModel.morphemes, suffixMorpheme)
-				memModel.morphemeByL2Tag[conjugation.Suffix+token.Tag] = suffixMorpheme
-			}
-			morphemes = []Morpheme{stemMorpheme, suffixMorpheme}
-		}
-
-		card, exists := memModel.cardByL2[token.Form]
-		if !exists {
-			card = Card{
-				Id:        memModel.getNextCardId(),
-				Type:      "VERB",
-				L2:        token.Form,
-				Morphemes: morphemes,
-			}
-		}
-		return card, nil
+		return memModel.VerbTokenToCard(token)
+	} else if token.IsNoun() || token.IsAdjective() {
+		return memModel.NounOrAdjectiveTokenToCard(token)
 	} else {
 		var type_ string
-		if token.IsAdjective() {
-			type_ = "ADJECTIVE"
-		} else if token.IsAdverb() {
+		if token.IsAdverb() {
 			type_ = "ADVERB"
 		} else if token.IsConjunction() {
 			type_ = "CONJUNCTION"
@@ -101,8 +208,6 @@ func (memModel *MemModel) TokenToCard(token parsing.Token) (Card, error) {
 			type_ = "DETERMINER"
 		} else if token.IsInterjection() {
 			type_ = "INTERJECTION"
-		} else if token.IsNoun() {
-			type_ = "NOUN"
 		} else if token.IsNumber() {
 			type_ = "NUMBER"
 		} else if token.IsPreposition() {
