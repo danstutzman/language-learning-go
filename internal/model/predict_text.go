fbf90e4076194e213b1bd8545bf03813397d4612
Dan Stutzman <dan@danstutzman.com>
2019-11-04 00:02:30 -0700

    Predictive text allows query without accent marks

--- internal/model/predict_text.go
+++ internal/model/predict_text.go
@@ -9,8 +9,30 @@ import (
 )
 
 type ProbWord struct {
-	prob float64
-	word string
+	prob          float64
+	word          string
+	wordNoAccents string
+}
+
+func removeAccent(r rune) rune {
+	switch r {
+	case 'á':
+		return 'a'
+	case 'é':
+		return 'e'
+	case 'í':
+		return 'i'
+	case 'ó':
+		return 'o'
+	case 'ú':
+		return 'u'
+	case 'ü':
+		return 'u'
+	case 'ñ':
+		return 'n'
+	default:
+		return r
+	}
 }
 
 func readProbWords(languageModelPath string) []ProbWord {
@@ -40,7 +62,10 @@ func readProbWords(languageModelPath string) []ProbWord {
 				panic(err)
 			}
 			word := values[1]
-			probWords = append(probWords, ProbWord{prob: prob, word: word})
+
+			wordNoAccents := strings.Map(removeAccent, word)
+			probWord := ProbWord{prob: prob, word: word, wordNoAccents: wordNoAccents}
+			probWords = append(probWords, probWord)
 		}
 	}
 
@@ -60,11 +85,20 @@ func (model *Model) PredictText(wordSoFar string) []string {
 		return []string{}
 	}
 	wordSoFarLower := strings.ToLower(wordSoFar)
+	wordSoFarLowerNoAccents := strings.Map(removeAccent, wordSoFarLower)
 
 	probWords := []ProbWord{}
-	for _, probWord := range model.probWords {
-		if strings.HasPrefix(probWord.word, wordSoFarLower) {
-			probWords = append(probWords, probWord)
+	if wordSoFarLowerNoAccents != wordSoFarLower { // if query has accents
+		for _, probWord := range model.probWords {
+			if strings.HasPrefix(probWord.word, wordSoFarLower) {
+				probWords = append(probWords, probWord)
+			}
+		}
+	} else {
+		for _, probWord := range model.probWords {
+			if strings.HasPrefix(probWord.wordNoAccents, wordSoFarLowerNoAccents) {
+				probWords = append(probWords, probWord)
+			}
 		}
 	}
 
